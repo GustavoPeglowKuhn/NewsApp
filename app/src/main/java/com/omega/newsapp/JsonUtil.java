e8d59f39284a987507d9d71449ba4639a37fecc3
GustavoKuhn <gustavowolgep@gmail.com>
2016-11-05 20:55:36 +0000

    testando json

---
+++ app/src/main/java/com/omega/newsapp/JsonUtil.java
@@ -0,0 +1,32 @@
+package com.omega.newsapp;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Omega on 2016-11-05.
+ */
+
+public class JsonUtil {
+    public static List<Item> fromJson(String json) throws JSONException {
+
+        List<Item> lista = new ArrayList<>();
+
+        JSONArray jsonArray = new JSONArray(json);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject object = (JSONObject) jsonArray.get(i);
+            String imagem = (String) object.get("imagem");
+            String titulo = (String) object.get("titulo");
+            String resumo = (String) object.get("resumo");
+            String data = (String) object.get("data");
+
+            lista.add(new Item(null, titulo, resumo, data));
+        }
+
+        return lista;
+    }
+}
--- app/src/main/java/com/omega/newsapp/MainActivity.java
+++ app/src/main/java/com/omega/newsapp/MainActivity.java
@@ -1,15 +1,23 @@
 package com.omega.newsapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView lista;
     private ItemAdapter adapter;
 
     @Override
@@ -18,14 +26,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        lista = (ListView) findViewById(R.id.lista);
+        ListView lista = (ListView) findViewById(R.id.lista);
 
-        List<Item> itemsList = new ArrayList<>();
+        /*List<Item> itemsList = new ArrayList<>();
 
-        itemsList.add(new Item(null, "001", "ipsum dolor sit amet, ad mei nullam graece alienum, vix fabellas\n" +
-                "vulputate ei. Meis luptatum singulis est ea. Pri id quot labore, suas\n" +
-                "melius quaeque cu eam. In suas aliquip pro. Eos ad everti percipitur\n" +
-                "interesset, fuisset consequuntur ut sea, labore vivendo eos te. No vel\n" +
+        itemsList.add(new Item(null, "001", "ipsum dolor sit amet, ad mei nullam graece alienum, vix fabellas" +
+                "vulputate ei. Meis luptatum singulis est ea. Pri id quot labore, suas" +
+                "melius quaeque cu eam. In suas aliquip pro. Eos ad everti percipitur" +
+                "interesset, fuisset consequuntur ut sea, labore vivendo eos te. No vel" +
                 "putent placerat, vel in malis deserunt.", "30/02/2017"));
         itemsList.add(new Item(null, "Titulo", "t eos solet detracto temporibus, eu docendi apeirian\n" +
                 "euripidis vim, eam atqui option id. Possim aperiam cum ne, qui ut ullum\n" +
@@ -33,11 +41,76 @@ public class MainActivity extends AppCompatActivity {
                 "partem, simul melius docendi ne sed. Eam te aperiri omittam\n" +
                 "interpretaris.", "30/02/2018"));
         itemsList.add(new Item(null, "Lorem Ipsum", "lorem Ipsum", "30/02/2019"));
-        itemsList.add(new Item(null, "Birl", "lorem Ipsum", "30/02/2020"));
+        itemsList.add(new Item(null, "Birl", "lorem Ipsum", "30/02/2020"));*/
 
-        adapter = new ItemAdapter(this, itemsList);
+        adapter = new ItemAdapter(this, new ArrayList<Item>());
+
         lista.setAdapter(adapter);
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                /*Intent intent = new Intent(MainActivity.this, DetalheActivity.class);
+                ItemVideo aula = (ItemVideo) parent.getItemAtPosition(position);
+                intent.putExtra("AULA", aula);
+                startActivity(intent);*/
+            }
+        });
+
+        new NewsTask().execute();
     }
 
+    class NewsTask extends AsyncTask<Void, Void, List<Item>> {
+
+        @Override
+        protected List<Item> doInBackground(Void... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            try {
+                //https://drive.google.com/file/d/0Bz8_rhlT4pQxQnJiSElOSFE0Y0k/view?usp=sharing
+                //https://drive.google.com/open?id=0Bz8_rhlT4pQxQnJiSElOSFE0Y0k
+
+                URL url = new URL("https://drive.google.com/file/d/0Bz8_rhlT4pQxQnJiSElOSFE0Y0k/view?usp=sharing");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String linha;
+                StringBuffer buffer = new StringBuffer();
+                while((linha = reader.readLine()) != null) {
+                    buffer.append(linha);
+                    buffer.append("\n");
+                }
+
+                return JsonUtil.fromJson(buffer.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(List<Item> itens) {
+            adapter.clear();
+            adapter.addAll(itens);
+            adapter.notifyDataSetChanged();
+        }
+    }
 
 }
